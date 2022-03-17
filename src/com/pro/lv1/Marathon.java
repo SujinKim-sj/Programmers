39ce02d0e8060269e2b8649e0efea4432f10be1c
SujinKim-sj <sjsjkim0@gmail.com>
2022-03-18 01:12:03 +0900

    Update Lv1_Marathon

--- src/com/pro/lv1/Marathon.java
+++ src/com/pro/lv1/Marathon.java
@@ -1,30 +1,36 @@
 package com.pro.lv1;
 
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class Marathon {
 	public static String solution(String[] participant, String[] completion) {
         String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
         
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-       
-        for(int i = 0; i < completion.length; i++) {
-        	if(!participant[i].equals(completion[i])) {
-        		answer = participant[i];
-        	}
-        	System.out.println(participant[i]
-        			);
+        //getOrDefault : 동일한 키가 있으면 participant[i]에 매핑되어있는 값 반환, 아니면 0 반환 
+        for(int i = 0; i < participant.length; i++) {
+        	//참가자 value에 1
+        	map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
         }
         
+        for(int i = 0; i < completion.length; i++) {
+        	//완주자 목록에 있으면 해당 완주자 value에 -1
+        	map.put(completion[i], map.get(completion[i]) - 1);
+        }
+        
+        //완주하지 못한 참가자는 value가 0이 아닌 값을 가진 참가자이다
+        for(int i = 0; i < participant.length; i++) {
+        	if(map.get(participant[i]) != 0) {
+        		answer = participant[i];
+        	}
+        }
         return answer;
     }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String[] participant = {"leo", "kiki", "eden"};
-		String[] completion = {"kiki", "eden"};
+		String[] completion = {"eden", "kiki"};
 		
-		System.out.println(solution(participant, completion));
+		System.out.println(solution(participant, completion));	//출력 : leo
 	}
 
 }
