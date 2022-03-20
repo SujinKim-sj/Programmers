3488508038bfdcd89e04b034b45538d53c9db976
Sujin Kim <69071772+SujinKim-sj@users.noreply.github.com>
2022-03-20 23:30:31 +0900

    Update 체육복.py

--- Level_1/체육복.py
+++ Level_1/체육복.py
@@ -1,1 +1,14 @@
+#그리디 알고리즘 Lv1. 체육복
 
+def solution(n, lost, reserve):
+    # 중복 제거
+    reserveSet = set(reserve) - set(lost)
+    lostSet = set(lost) - set(reserve)
+
+    for i in reserveSet:
+        if i - 1 in lostSet:
+            lostSet.remove(i - 1)
+        elif i + 1 in lostSet:
+            lostSet.remove(i + 1)
+
+    return n - len(lostSet)
