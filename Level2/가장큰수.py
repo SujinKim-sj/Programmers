ed7c455cfe5533afdebe944908d0c88d9bc6be02
Sujin Kim <69071772+SujinKim-sj@users.noreply.github.com>
2022-04-01 16:38:02 +0900

    Create 가장큰수.py

---
+++ Level2/가장큰수.py
@@ -0,0 +1,6 @@
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    print(numbers)
+    numbers.sort(key=lambda x: x * 3, reverse=True)
+    # print(numbers)
+    return str(int(''.join(numbers)))
