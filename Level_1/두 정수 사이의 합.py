80cdf04b96863a8a26f94d55e482902e226009cb
SujinKim-sj <sjsjkim0@gmail.com>
2023-06-02 00:02:03 +0900

    연습문제 Lv1 - 두 정수 사이의 합

--- Level_1/두 정수 사이의 합.py
+++ Level_1/두 정수 사이의 합.py
@@ -11,3 +11,13 @@ def solution(a, b):
             answer += i
 
     return answer
+
+
+# 다른 사람의 풀이
+def adder(a, b):
+    if a > b:
+        a, b = b, a
+    return sum(range(a, b + 1))
+
+def adder2(a, b):
+    return (abs(a-b)+1)*(a+b)//2        # 절댓값 사용
