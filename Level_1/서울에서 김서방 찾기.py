f1d3ef7ff5926088d0978ff5354ce2da81df3167
SujinKim-sj <sjsjkim0@gmail.com>
2023-06-20 21:18:35 +0900

    연습문제 Lv1 - 서울에서 김서방 찾기

---
+++ Level_1/서울에서 김서방 찾기.py
@@ -0,0 +1,8 @@
+def solution(seoul):
+    answer = ''
+    for i in seoul:
+        if i == "Kim":
+            answer = "김서방은 " + str(seoul.index(i)) + "에 있다"
+    return answer
+
+print(solution(["Jane", "Kim"]))
