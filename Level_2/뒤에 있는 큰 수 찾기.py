87ecf5c3056f2d5eeeabf0d73adca7c5037c7631
SujinKim-sj <sjsjkim0@gmail.com>
2023-10-12 00:07:52 +0900

    연습문제 - 뒤에 있는 큰 수 찾기

---
+++ Level_2/뒤에 있는 큰 수 찾기.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    answer = [-1 for i in range(len(numbers))]
+    stack = []
+
+    for i in range(len(numbers)):
+        while stack and numbers[stack[-1]] < numbers[i]:
+            answer[stack.pop()] = numbers[i]
+        stack.append(i) # 인덱스
+
+    return answer
