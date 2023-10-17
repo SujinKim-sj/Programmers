1a5c602714bbaa84bfe16b7f13076dd3f5cff5f2
SujinKim-sj <sjsjkim0@gmail.com>
2023-10-17 21:49:47 +0900

    스택

---
+++ 고득점키트/스택,큐/Lv2_기능개발.py
@@ -0,0 +1,21 @@
+def solution(progresses, speeds):
+    answer = []
+    cnt = 0
+    day = 0
+
+    while len(progresses) != 0:
+        tmp = progresses[0] + (day * speeds[0])
+        if tmp >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+
+            cnt += 1
+        else:
+            if cnt > 0:
+                answer.append(cnt)
+                cnt = 0
+            else:
+                day += 1
+
+    answer.append(cnt)
+    return answer
